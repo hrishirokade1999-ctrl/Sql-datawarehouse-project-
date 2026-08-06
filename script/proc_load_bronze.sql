64b099964d016ba7e4656fbc1b4a7b135f76f50a
hrishirokade1999-ctrl <hrishirokade1999@gmail.com>
2026-08-06 23:54:52 +0530

    Enhance bronze.load_bronze procedure with print statements
    Added print statements for loading processes and structured the procedure for better readability.

--- script/proc_load_bronze.sql
+++ script/proc_load_bronze.sql
@@ -1,6 +1,16 @@
 create or alter Procedure  bronze.load_bronze as
 BEGIN
+PRINT '======================================================'
+PRINT '==============LODING Bronze Layer====================='
+PRINT '======================================================'
+
+Print '------------------------------------------------------'
+print 'LODING CRM Tables.....'
+print '------------------------------------------------------'
+
+print '>>Trunacting Table: bronze.crm_cust_info'
 truncate table bronze.crm_cust_info
+print 'Inserting Data Into: bronze.crm_cust_info'
 bulk insert bronze.crm_cust_info 
 from 'C:\Users\Admin\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
 with (
@@ -10,8 +20,9 @@ tablock
 );
 
 
-
+print '>>Trunacting Table: bronze.crm_prd_info'
 truncate table bronze.crm_prd_info
+print 'Inserting Data Into: bronze.crm_prd_info'
 bulk insert bronze.crm_prd_info
 from 'C:\Users\Admin\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
 with (
@@ -21,8 +32,9 @@ tablock
 );
 
 
-
+print '>>Trunacting Table: bronze.crm_sales_details'
 truncate table bronze.crm_sales_details
+print 'Inserting Data Into: bronze.crm_sales_details'
 bulk insert bronze.crm_sales_details
 from 'C:\Users\Admin\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
 with (
@@ -32,7 +44,13 @@ tablock
 );
 
 
+Print '------------------------------------------------------'
+print 'LODING ERP Tables.....'
+print '------------------------------------------------------'
+
+print '>>Trunacting Table: bronze.erp_cust_az12'
 truncate table bronze.erp_cust_az12
+print 'Inserting Data Into: bronze.erp_cust_az12'
 bulk insert bronze.erp_cust_az12
 from 'C:\Users\Admin\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\cust_az12.csv'
 with (
@@ -42,8 +60,9 @@ tablock
 );
 
 
-
+print '>>Trunacting Table: bronze.erp_loc_a101'
 truncate table bronze.erp_loc_a101
+print 'Inserting Data Into: bronze.erp_loc_a101'
 bulk insert bronze.erp_loc_a101
 from 'C:\Users\Admin\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\loc_a101.csv'
 with (
@@ -52,8 +71,9 @@ fieldterminator =',',
 tablock
 );
 
-
+print '>>Trunacting Table: bronze.erp_px_cat_g1v2'
 truncate table bronze.erp_px_cat_g1v2
+print 'Inserting Data Into: bronze.erp_px_cat_g1v2'
 bulk insert bronze.erp_px_cat_g1v2
 from 'C:\Users\Admin\Downloads\sql-data-warehouse-project\sql-data-warehouse-project\datasets\source_erp\px_cat_g1v2.csv'
 with (
